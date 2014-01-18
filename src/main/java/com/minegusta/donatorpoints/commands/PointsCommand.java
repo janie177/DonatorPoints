57315e130526db58e3a21b97dd9934e241144953
Jan <janie177@hotmail.com>
2014-01-18 13:10:42 +0100

    Redone /Points command.

--- src/main/java/com/minegusta/donatorpoints/commands/PointsCommand.java
+++ src/main/java/com/minegusta/donatorpoints/commands/PointsCommand.java
@@ -1,60 +1,102 @@
 package com.minegusta.donatorpoints.commands;
 
+import com.google.common.collect.Lists;
 import com.minegusta.donatorpoints.data.DataManager;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.List;
+
 public class PointsCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender s, Command cmd, String label, String[] args) {
         if (cmd.getName().equalsIgnoreCase("points")) {
+            if (s instanceof ConsoleCommandSender) return true;
             Player p = (Player) s;
-            if (args.length == 0) {
-                if (s instanceof ConsoleCommandSender) return true;
-                if (DataManager.getPointsFromPlayer(p) == null) DataManager.setPointsFromPlayer(p, 0);
-                p.sendMessage(ChatColor.AQUA + "You have " + ChatColor.LIGHT_PURPLE + DataManager.getPointsFromPlayer(p) + ChatColor.AQUA + " points.");
-                return true;
-            } else if (args.length == 1 || args.length == 2) {
-                if (s instanceof ConsoleCommandSender) return true;
-                p.sendMessage(ChatColor.AQUA + "[Points]" + ChatColor.GRAY + " Use" + ChatColor.GOLD + " /Points " + ChatColor.GRAY + "To display your points.");
-                p.sendMessage(ChatColor.AQUA + "[Points]" + ChatColor.GRAY + " Use" + ChatColor.GOLD + " /Points Set <Name> <Amount> " + ChatColor.GRAY + "To set someone's points.");
-                p.sendMessage(ChatColor.AQUA + "[Points]" + ChatColor.GRAY + " Use" + ChatColor.GOLD + " /Points Add <Name> <Amount> " + ChatColor.GRAY + "To add some points to a player.");
-            } else if (args.length == 3 && args[0].equalsIgnoreCase("Set")) {
-                if (!p.isOp()) return true;
-                if (s instanceof ConsoleCommandSender) return true;
-                try {
-                    OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(args[1]);
-                    Player player = (Player) offlinePlayer;
-                    int amount = Integer.parseInt(args[2]);
-                    DataManager.setPointsFromPlayer(player, amount);
-                    p.sendMessage(ChatColor.AQUA + "[Points]" + ChatColor.GRAY + " Points for " + player.getName() + " successfully set to " + amount + ".");
-                } catch (Exception e) {
-                    p.sendMessage(ChatColor.AQUA + "[Points]" + ChatColor.RED + " Player not found or not a number.");
-                    p.sendMessage(ChatColor.AQUA + "[Points]" + ChatColor.GRAY + " Use" + ChatColor.GOLD + " /Points Set <Name> <Amount> " + ChatColor.GRAY + "To set someone's points.");
-                    return true;
-                }
-            } else if (args.length == 3 && args[0].equalsIgnoreCase("Add")) {
-                try {
-                    if (!p.isOp()) return true;
-                    OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(args[1]);
-                    Player player = (Player) offlinePlayer;
-                    int amount = Integer.parseInt(args[2]);
-                    final int oldPoints = DataManager.getPointsFromPlayer(player);
-                    DataManager.setPointsFromPlayer(player, amount + oldPoints);
-                    player.sendMessage(ChatColor.AQUA + "[Points] " + ChatColor.AQUA + amount + ChatColor.GRAY + " Points added!");
-                } catch (Exception e) {
-                    return true;
-                }
+            int points = Integer.parseInt(args[2]);
+            int senderPoints = DataManager.getPointsFromPlayer(p);
+            Player receiver = Bukkit.getOfflinePlayer(args[1]).getPlayer();
+            int receiverPoints = DataManager.getPointsFromPlayer(receiver);
 
 
+            List<String> help = Lists.newArrayList("Your currently have " + ChatColor.AQUA + DataManager.getPointsFromPlayer(p) + ChatColor.YELLOW + "Points.", ChatColor.GOLD + " - - - - - - - - - - - - - ", "/Points" + ChatColor.GRAY + " - Shows this menu", "/Points Pay <Name> <Amount>" + ChatColor.GRAY + " - Pay a player points.", "/Points <Name>" + ChatColor.GRAY + " - See someone's points.", "/Points Set <Name> <Amount>" + ChatColor.GRAY + " - Set someone's points.", "/Points Add <Name> <Amount>" + ChatColor.GRAY + " - Add points to a player.");
+            List<String> insufficientFunds = Lists.newArrayList(ChatColor.RED + "You do not have that many points!");
+            List<String> wrongPlayer = Lists.newArrayList(ChatColor.RED + "Player is not online or does not exist!");
+            List<String> success = Lists.newArrayList("Sucessfully added " + ChatColor.AQUA + points + ChatColor.YELLOW + " points to " + receiver.getName());
+            List<String> pointsFromPlayer = Lists.newArrayList(receiver.getName() + " has " + ChatColor.AQUA + receiverPoints + ChatColor.YELLOW + " points.");
+
+
+            if (args.length == 0) sendText(p, help);
+            else if (args[0].equalsIgnoreCase("Pay") || args[0].equalsIgnoreCase("give")) {
+                if (args.length < 3) sendText(p, help);
+                else {
+
+
+                    for (Player player : Bukkit.getOnlinePlayers()) {
+                        if (player.equals(receiver)) {
+                            if (points > senderPoints) sendText(p, insufficientFunds);
+                            else {
+
+                                DataManager.setPointsFromPlayer(receiver, receiverPoints + points);
+                                DataManager.setPointsFromPlayer(p, senderPoints - points);
+                                sendText(p, success);
+                                return true;
+                            }
+                        }
+                    }
+                    sendText(p, wrongPlayer);
+
+                }
+
+            } else if (args[0].equalsIgnoreCase("add")) {
+                if (args.length < 3) sendText(p, help);
+                else {
+                    for (Player player : Bukkit.getOnlinePlayers()) {
+                        if (player.equals(receiver)) {
+                            DataManager.setPointsFromPlayer(receiver, receiverPoints + points);
+                            sendText(p, success);
+                            return true;
+                        }
+                    }
+                }
+                sendText(p, wrongPlayer);
+            } else if (args[0].equalsIgnoreCase("set")) {
+                if (args.length < 3) sendText(p, help);
+                else {
+                    for (Player player : Bukkit.getOnlinePlayers()) {
+                        if (player.equals(receiver)) {
+                            DataManager.setPointsFromPlayer(receiver, points);
+                            sendText(p, success);
+                            return true;
+                        }
+                    }
+                }
+                sendText(p, wrongPlayer);
+            } else if (args.length == 1) {
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    if (player.equals(receiver)) {
+                        sendText(p, pointsFromPlayer);
+                    }
+                }
+                sendText(p, wrongPlayer);
+
+            } else {
+                sendText(p, help);
             }
         }
         return true;
+    }
+
+    private void sendText(Player p, List<String> text) {
+        p.sendMessage(ChatColor.DARK_RED + "---------------" + ChatColor.GOLD + " Points Help " + ChatColor.DARK_RED + "---------------");
+        for (String s : text) {
+            p.sendMessage(ChatColor.YELLOW + s);
+        }
+        p.sendMessage(ChatColor.DARK_RED + "------------------------------------------");
     }
 }
