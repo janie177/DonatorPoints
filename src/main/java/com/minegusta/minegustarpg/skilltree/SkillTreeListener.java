5185d14c212ea31bfaefa98ab8ba3ee48fd27dff
Jan <janie177@hotmail.com>
2014-02-02 20:32:02 +0100

    Fixed cooldown message not showing at EntityInteractEntity event.

--- src/main/java/com/minegusta/minegustarpg/skilltree/SkillTreeListener.java
+++ src/main/java/com/minegusta/minegustarpg/skilltree/SkillTreeListener.java
@@ -426,7 +426,7 @@ public class SkillTreeListener implements Listener {
             if (healerCooldown.containsKey(p.getUniqueId().toString())) {
                 long remainingTime = System.currentTimeMillis() - healerCooldown.get(p.getUniqueId().toString());
                 if (!(remainingTime >= coolDownTime)) {
-                    p.sendMessage(ChatColor.YELLOW + "You have to wait another " + ChatColor.RED + getRemainingCooldown(remainingTime - remainingTime) + ChatColor.YELLOW + " before you can heal again.");
+                    p.sendMessage(ChatColor.YELLOW + "You have to wait another " + ChatColor.RED + getRemainingCooldown(coolDownTime - remainingTime) + ChatColor.YELLOW + " before you can heal again.");
                     return;
                 }
             }
